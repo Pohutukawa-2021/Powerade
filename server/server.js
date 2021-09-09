05298eeffd25a4f1b95fa3f9d1ffcc5c320913c6
csiyang <csiyang@yahoo.co.nz>
2021-09-10 10:48:59 +1200

    pulling from main

--- server/server.js
+++ server/server.js
@@ -6,4 +6,16 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
+server.get('/messages', (req, res) => {
+
+})
+
+server.post('/messages', (req, res) => {
+
+})
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
 module.exports = server
