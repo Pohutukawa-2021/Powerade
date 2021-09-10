b649f58ea2eb96943fbf8d5b400866cc9565b682
nzalancheng <85937572+nzalancheng@users.noreply.github.com>
2021-09-10 12:42:24 +1200

    chatroom incomplete

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Home from './Home'
+import Login from './Login'
+import User2 from './User2'
+import Chatroom from './Chatroom'
+import { Route } from 'react-router-dom'
 
-import Words from './Words'
-import AddWord from './AddWord'
-
-const App = () => (
-  <div className='app-container'>
-    <Words />
-    <AddWord />
-  </div>
-)
+function App () {
+  return (
+    <>
+      <div className='app'></div>
+      <Route path='/' component={Home} />
+      <Route path='/login' component={Login} />
+      <Route path='/User2' component={User2} />
+      <Route path='/Chatroom' component={Chatroom} />
+    </>
+  )
+}
 
 export default App
---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home () {
+  return (
+    <div className='Home'>
+      <p> Powerade</p>
+      <p> Powerade - the power that brings people closer</p>
+      <img src=''></img>
+    </div>
+  )
+}
+
+export default Home
