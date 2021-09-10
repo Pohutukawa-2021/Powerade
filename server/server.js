10354a934db311c0af683fe9374d553cc9e3aa90
csiyang <csiyang@yahoo.co.nz>
2021-09-10 13:52:31 +1200

    completed route and DB to veiew and send messages

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
+const db = require('./db/db')
+
 const path = require('path')
 const express = require('express')
-
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.get('/messages', (req, res) => {
-
+// Change to post
+// pass this an object with property of sender id and receiver id
+server.post('/messages/view', (req, res) => {
+  db.getMessages(req.body)
+    .then(message => {
+      res.json(message)
+      return null
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR', err.message)
+    })
 })
 
-server.post('/messages', (req, res) => {
-
+server.post('/messages/send', (req, res) => {
+  db.newMessage(req.body)
+    .then(() => {
+      res.status(201).send('Message Sent')
+      return null
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR' + err.message)
+    })
 })
 
 server.get('*', (req, res) => {
